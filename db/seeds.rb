dea2c908840fc2db383f8711e21cca5669c21ac2
YANNI <yanniflambas@hotmail.com>
2020-01-13 16:09:55 +1100

    changed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
 User.destroy_all
+puts "Creating users"
 
 u1 = User.create! name:'yanni', email: 'yanni@test.com', password: 'chicken', admin: 'true'
-c1 = Character.create! name:'Woahmen', user: u1
 
 puts "Created #{ User.count } users."
-puts "Linked #{ Character }s  "
+
+# Character.destroy_all
+# puts "Creating characters"
+#
+# c1 = Character.create! name: 'Woahmen', user_id: u1.id
+#
+# puts "Linked #{ Character.count } characters. "
