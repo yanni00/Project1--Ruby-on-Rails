52900f276ba8c6a8399f76d6b9d69cc753620fe6
YANNI <yanniflambas@hotmail.com>
2020-01-15 15:23:57 +1100

    Added image uploading with Cloudinary Revised

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,31 +1,35 @@
 class CharactersController < ApplicationController
+  def index
+    @characters = Character.where :user => @current_user
+  end
+
+  def show
+    @character = Character.find params[:id]
+  end
 
   def new
     @character = Character.new
   end
 
   def create
-    # character = Character.find_by name: params[:name]
-    # raise 'hell'
-    # Create the new character using the params passed through from
-    # the form
+    character = Character.new character_params
+
+    if params[:file].present?
+      req = Cloudinary::Uploader.upload(params[:file])
+      character.image = req["public_id"]
+    end
+    character.save
+
     @character = Character.create name: params[:character][:name], server: params[:character][:server], spec: params[:character][:spec], subspec: params[:character][:subspec], user: @current_user
 
     # redirect_to character_path(character.id)
 
     redirect_to user_path(@current_user.id)
 
-
-  end
-
-  def index
-    @character = Character.where :user => @current_user
   end
 
 
 
-  def show
-  end
 
   def update
   end
@@ -33,5 +37,9 @@ class CharactersController < ApplicationController
   def destroy
   end
 
+  private
+  def character_params
+    params.require(:character).permit(:name)
+  end
 
 end
